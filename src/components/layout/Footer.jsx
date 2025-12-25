769cc828504ff9688a47da92db1b14e6ffcd0676
Matheus do Note <alvesdemelomatheus4@gmail.com>
2025-12-25 15:05:18 -0300

    Criação da

--- src/components/layout/Footer.jsx
+++ src/components/layout/Footer.jsx
@@ -1,6 +1,6 @@
 import { FaFacebook, FaInstagram, FaLinkedin } from "react-icons/fa";
 
-import styles from './Footer.modules.css'
+import styles from './Footer.module.css'
 function Footer() {
   return (
     <footer className={styles.footer}>
