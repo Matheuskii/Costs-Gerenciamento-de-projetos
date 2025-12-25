4fa85c01e4c02b8732999de4457a6923e862faf3
Matheus do Note <alvesdemelomatheus4@gmail.com>
2025-12-25 12:22:01 -0300

    adicionando página home completa e iniciando a tela newproject

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -1,4 +1,17 @@
+import styles from '.Home.modules.css'
+import savings from '../../img/savings.svg'
+import LinkButton from '../layout/LinkButton'
 function Home (){
-    return <h1>Home</h1>
+    return (
+        <section className={styles.home_container}>
+        <h1>Bem-vindo ao <span>Costs</span></h1>
+        <p>Comece a gerenciar os seus projetos agora mesmo!</p>
+        <LinkButton to='/newproject' text="Criar Projetos" />
+        <img src={savings} alt="Costs" /></section>
+    
+
+
+
+    )
 }
 export default Home
