e1d3114c55a1194e8c62c37ec9f3a5bc71d7c2ce
Matheus do Note <alvesdemelomatheus4@gmail.com>
2025-12-25 15:41:48 -0300

    fix: imports css and select

--- src/components/form/Select.jsx
+++ src/components/form/Select.jsx
@@ -1,15 +1,26 @@
-
-import styles from "./Select.module.css";
+import styles from './Select.module.css';
 
 function Select({ text, name, options, handleOnChange, value }) {
   return (
     <div className={styles.form_control}>
       <label htmlFor={name}>{text}:</label>
-      <select name={name} id={name}>
-        <option >Selecione uma opção</option>
-        {options.map((option) =>(
-            console.log(option)
-        ))}
+      <select 
+        name={name} 
+        id={name} 
+        onChange={handleOnChange} 
+        value={value}
+      >
+        <option>Selecione uma opção</option>
+        {options &&
+          options.map((option) => {
+            // Optional: You can keep the console log here inside the block if you want
+            // console.log(option); 
+            return (
+              <option value={option.id} key={option.id}>
+                {option.Nome}
+              </option>
+            );
+          })}
       </select>
     </div>
   );
--- src/components/layout/LinkButton.jsx
+++ src/components/layout/LinkButton.jsx
@@ -1,4 +1,4 @@
-import styles from './LinkButton.modules.css'
+import styles from './LinkButton.module.css'
 import {Link} from 'react-router-dom'
 function LinkButton({to, text}) {
     return (
