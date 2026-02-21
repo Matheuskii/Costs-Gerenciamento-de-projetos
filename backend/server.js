501972c329ee0a3409d5215f68fce35dcc4e6996
Matheus <alvesdemelomatheus4@gmail.com>
2026-02-21 17:19:29 -0300

    feat: adjust CORS policies and environment variables

--- backend/server.js
+++ backend/server.js
@@ -6,9 +6,11 @@ const PORT = process.env.PORT || 5000;
 
 
 const app = express()
-app.use(cors({ 
-    origin: process.env.CLIENT_URL || 'http://localhost:3000' 
-}))
+app.use(cors({
+  origin: process.env.CLIENT_URL, 
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
+  credentials: true
+}));
 app.use(bodyParser.json())
 
 app.get('/categories', async (req, res) => {
